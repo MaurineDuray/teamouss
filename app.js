21ce6d4c02b41e12f168aafb345854b9233dfdce
MaurineDuray <maurine.duray@skynet.be>
2022-06-16 11:06:53 +0200

    terminus

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function loader(){
     const TLLOAD = gsap.timeline();
 
     TLLOAD
- .to('.load-container',{duration: 2, delay: 7, opacity: 0, display: 0});
+ .to('.load-container',{duration: 2, delay: 5, opacity: 0, display: 0});
 
  TLLOAD.add(()=>{
         document.querySelector('.load-container').style.display= "none";
